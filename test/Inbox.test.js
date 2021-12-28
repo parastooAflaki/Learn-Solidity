a35400ced0ba2f61c496c79bcd0f44e35fbed1e5
parastooAflaki <parastoofalakaflaki@gmail.com>
2021-12-28 23:04:06 +0000

    refactor to Async/Await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,15 +8,13 @@ const web3  = new Web3(ganache.provider()); // instance of Web3
 //provider is responsible for communication betweeb web3 instance and the network its connected to
 
 
+let accounts;
 //Get unlocked accounts : accounts created by ganache to send/recieve ether on test network without concerning over private keys.
-beforeEach( ()=>{
+beforeEach( async ()=>{
     // Get a list of all acounts    
-     web3.eth.getAccounts()
-     .then(fetchedAccounts => {
-         console.log(fetchedAccounts)
-     })
+    accounts = await web3.eth.getAccounts();
      
-      //web3 can be used for different types of cryptos. eth module for working with ethereum.
+    //web3 can be used for different types of cryptos. eth module for working with ethereum.
 
 
     // Use one of the accounts to deploy the contract
@@ -24,7 +22,9 @@ beforeEach( ()=>{
 
 
 describe('Inbox' , ()=>{
-    it('deploys a contract' , ()=>{})
+    it('deploys a contract' , ()=>{
+        console.log(accounts);
+    })
 })
 
 
