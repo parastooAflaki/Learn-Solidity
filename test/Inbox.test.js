98675604cd70a319a7fd068bf6f3dada4e7a263d
parastooAflaki <parastoofalakaflaki@gmail.com>
2021-12-28 23:46:11 +0000

    create and deploy the first contract!

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,23 +7,26 @@ const Web3 = require('web3'); // Use ut to create instances -> constructor -> Up
 const web3  = new Web3(ganache.provider()); // instance of Web3
 //provider is responsible for communication betweeb web3 instance and the network its connected to
 
+const {interface , bytecode} = require('../compile.js')
+
 
 let accounts;
+let inbox;
 //Get unlocked accounts : accounts created by ganache to send/recieve ether on test network without concerning over private keys.
 beforeEach( async ()=>{
     // Get a list of all acounts    
     accounts = await web3.eth.getAccounts();
-     
-    //web3 can be used for different types of cryptos. eth module for working with ethereum.
-
 
     // Use one of the accounts to deploy the contract
-})
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data : bytecode , arguments : ['Hi There!!']})
+    .send({ from: accounts[0] , gas : '1000000'})
+});
 
 
 describe('Inbox' , ()=>{
     it('deploys a contract' , ()=>{
-        console.log(accounts);
+        console.log(inbox);
     })
 })
 
