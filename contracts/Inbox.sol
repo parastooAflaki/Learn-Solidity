3b2658a8effd5db2cfd78112dc4437013e1e517f
parastooAflaki <parastoofalakaflaki@gmail.com>
2021-12-28 19:18:50 +0000

    error resolved

---
+++ contracts/Inbox.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.17;
+
+contract Inbox{
+
+    string public message;
+    function Inbox( string initialMessage ) public{
+        message = initialMessage;
+    }
+
+    function setMessage( string newMessage) public{
+        message = newMessage;    
+    }
+
+    function doMath(int a , int b) public{
+    
+        a+b;
+        a-b;
+        a*b;
+        a==0;
+        
+    }
+}
---
+++ test/Inbox.test.js
@@ -0,0 +1,48 @@
+// contract test code will go here
+const assert = require('assert'); //assert that some value is equal some other value
+
+const ganache = require('ganache-cli');
+
+const Web3 = require('web3'); // Use ut to create instances -> constructor -> Uppercase W
+const web3  = new Web3(ganache.provider()); // instance of Web3
+//provider is responsible for communication betweeb web3 instance and the network its connected to
+
+//mocha is test running framework. used for backend, frontend and even solidity
+
+//Below is an example class used for learning how to test with mocha
+
+class Car{
+
+    park(){
+        return 'stopped';
+    }
+
+    drive(){
+        return 'vroom';
+    }
+}
+
+// The first string in describe can be anything. not essentially the name of the class. Just for the output report
+// Secpnd arg is an arrow function
+describe('Car' , ()=>{
+    // Again first string just describes the purpose for further output.
+    it('if park should return a string!' , ()=>{
+      //   const car = new Car();
+         assert.equal( car.park() , 'stopped' );
+    });
+
+    it('if car can drive' , ()=>{
+       // const car = new Car();
+        assert.equal(car.drive() , 'vroom')
+    })
+});
+
+
+let car;
+// before execute each it function, it will be runned
+beforeEach(()=>{
+    car = new Car();
+})
+//How to run mocha in this project?
+//Make sure to change the 'test' in scripts in package.json to 'mocha'
+// in terminal run 'npm run test'
