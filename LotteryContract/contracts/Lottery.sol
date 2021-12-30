79e387127051c88a031c605e08df0b370d264f82
parastooAflaki <parastoofalakaflaki@gmail.com>
2021-12-30 16:32:36 +0000

    requiring managers and resetting contract state!

--- LotteryContract/contracts/Lottery.sol
+++ LotteryContract/contracts/Lottery.sol
@@ -20,8 +20,11 @@ contract lottery{
     }
 
     function pickWinner() public {
+        require(msg.sender == manager);
         uint index = random() % players.length;
         players[index].transfer(this.balance);
+
+        players = new address[](0);
     }
 
 
