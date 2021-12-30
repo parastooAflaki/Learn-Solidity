abe43e88c9703fdf3c1e79c95a445689c56205c0
parastooAflaki <parastoofalakaflaki@gmail.com>
2021-12-30 14:44:10 +0000

    Sending money to the winner!

---
+++ LotteryContract/contracts/Lottery.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.17;
+
+contract lottery{
+
+    address public manager;
+    address[] public players;
+
+    function lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns(uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    
+    }
+
+    function pickWinner() public {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+    }
+
+
+}
